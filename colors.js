52c89dd831f291f70df269b61076d37cac2c757b
LimHyungang <47623969+LimHyungang@users.noreply.github.com>
2020-03-07 14:57:20 +0900

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,35 @@
+var Body = {
+  setColor:function (color) {
+    document.querySelector('body').style.color = color;
+  },
+
+  setBackgroundColor:function (color) {
+    document.querySelector('body').style.backgroundColor = color;
+  }
+}
+
+var Link = {
+  setColor:function (color) {
+    var alist = document.querySelectorAll('a');
+    for (var i = 0; i < alist.length; i++) {
+      alist[i].style.color = color;
+    }
+  }
+}
+
+function nightDayHandler (self) {
+  var target = document.querySelector('body');
+  if (self.value === 'night') {
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+
+    Link.setColor('powderblue');
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+
+    Link.setColor('blue');
+  }
+}
