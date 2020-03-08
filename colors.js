8932781edc8cce416ff4ffe21e34f410cd2fd30e
LimHyungang <47623969+LimHyungang@users.noreply.github.com>
2020-03-08 11:51:48 +0900

    주석 추가

--- colors.js
+++ colors.js
@@ -1,27 +1,20 @@
 var Body = {
   setColor:function (color) {
-    // document.querySelector('body').style.color = color;
-    $('body').css('color', color);
+    $('body').css('color', color);  // body태그의 color를 매개변수로 받은 color로 변경  
   },
 
   setBackgroundColor:function (color) {
-    // document.querySelector('body').style.backgroundColor = color;
-    $('body').css('backgroundColor', color);
+    $('body').css('backgroundColor', color);  // body태그의 backgroundColor를 매개변수로 받은 color로 변경
   }
 }
 
 var Link = {
   setColor:function (color) {
-    // var alist = document.querySelectorAll('a');
-    // for (var i = 0; i < alist.length; i++) {
-    //   alist[i].style.color = color;
-    // }
-    $('a').css('color', color);
+    $('a').css('color', color);  // 모든 a태그의 color를 매개변수로 받은 color로 변경
   }
 }
 
-function nightDayHandler (self) {
-  var target = document.querySelector('body');
+function nightDayHandler (self) {  // #bright버튼의 정보에 따라 주야간모드 적용
   if (self.value === 'night') {
     Body.setBackgroundColor('black');
     Body.setColor('white');
